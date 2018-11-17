82a1b598594e50af1dc9592a1b30a25fe1779f36
Paul Coroneos <paulalexandercoroneos@gmail.com>
2018-11-17 08:52:56 -0600

    Minimum requirements met. TODO: Make a dark modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,55 @@
+//seeding some values
+
 var friendsData = [
     {
-        "name":"Elmo",
-        "photo":"https://upload.wikimedia.org/wikipedia/en/7/74/Elmo_from_Sesame_Street.gif",
+        "name":"Darth Vader",
+        "photo":"https://www.soy502.com/sites/default/files/styles/full_node/public/2017/Mayo/04/darth-vader_6bda9114.jpg",
         "scores":[
-            5,
             1,
-            4,
-            4,
-            5,
             1,
+            5,
+            3,
             2,
+            3,
+            1,
             5,
-            4,
+            3,
             1
           ]
-      }
+      },
+    {
+        "name":"Yoda",
+        "photo":"https://upload.wikimedia.org/wikipedia/en/thumb/6/6f/Yoda_Attack_of_the_Clones.png/170px-Yoda_Attack_of_the_Clones.png",
+        "scores":[
+            5,
+            5,
+            1,
+            3,
+            1,
+            1,
+            3,
+            2,
+            1,
+            4
+        ]
+    },
+    {
+        "name":"Luke",
+        "photo":"https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Luke_Skywalker.png/220px-Luke_Skywalker.png",
+        "scores":[
+            5,
+            5,
+            2,
+            5,
+            1,
+            1,
+            1,
+            2,
+            1,
+            5
+        ]
+    }
+
   ];
   
   module.exports = friendsData;
